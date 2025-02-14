37dcf3c2683b5b4f90ba82e8e56b0d98746922de
anabelena <anabelen.aristah@gmail.com>
2025-02-13 18:14:16 -0800

    feat: create db conection, add type for Product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import {
   DropdownMenu,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, Filter } from "lucide-react";
 import { DropdownMenuContent } from "@radix-ui/react-dropdown-menu";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
@@ -17,30 +17,24 @@ const SORT_OPTIONS = [
 ] as const;
 
 export default function Home() {
-
+  
   const [filter, setFilter] = useState({ sort: "none" });
-
 
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
+      <div className="flex items-baseline justify-between border-b-2 border-gray-300 pb-6 pt-24">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900">
           High-quality selection
         </h1>
 
         <div className="flex items-center">
-
           <DropdownMenu>
-
             <DropdownMenuTrigger className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
               Sort
               <ChevronDown className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
-              
             </DropdownMenuTrigger>
 
-
             <DropdownMenuContent align="end">
-
               {SORT_OPTIONS.map((option) => (
                 <button
                   key={option.name}
@@ -55,16 +49,14 @@ export default function Home() {
                   {option.name}
                 </button>
               ))}
-              
             </DropdownMenuContent>
-
           </DropdownMenu>
+
+          <button className="-mr-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden">
+            <Filter className="h-5 w-5" />
+          </button>
         </div>
       </div>
-
-      
-
-      
     </main>
   );
 }
